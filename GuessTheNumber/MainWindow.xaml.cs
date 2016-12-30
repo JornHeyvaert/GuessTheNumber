a52faf24270b6ea90f353f89e0db595c7c7a08e7
Jorn Heyvaert <jorn.heyvaert@telenet.be>
2016-12-30 20:54:04 +0100

    Initial commit

--- GuessTheNumber/MainWindow.xaml.cs
+++ GuessTheNumber/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GuessTheNumber
 {
@@ -20,9 +10,106 @@ namespace GuessTheNumber
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int numberToGuess = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (!rbEasy.IsChecked.Value && !rbMedium.IsChecked.Value && !rbHard.IsChecked.Value && !rbVeryHard.IsChecked.Value)
+            {
+                lblMessage.Text = "You must choose a difficulty!!!";
+                btnValidate.IsEnabled = false;
+                txtGuessNumber.Text = string.Empty;
+                lblGeneratedNumber.Content = string.Empty;
+            }
+            else
+            {
+                btnNewGame.IsEnabled = false;
+                btnValidate.IsEnabled = true;
+                txtGuessNumber.Text = string.Empty;
+                lblMessage.Text = string.Empty;
+                lblGeneratedNumber.Content = string.Empty;
+                EnableDifficulty(false);
+                numberToGuess = GenerateNumber();
+            }
+        }
+
+        private void btnValidate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtGuessNumber.Text))
+            {
+                lblMessage.Text = (Convert.ToInt32(txtGuessNumber.Text) == numberToGuess) ? "You Win!!!" : "You Lose!!!";
+                lblGeneratedNumber.Content = numberToGuess.ToString();
+                btnValidate.IsEnabled = false;
+                btnNewGame.IsEnabled = true;
+                ResetDifficulty();
+                EnableDifficulty(true);
+            }
+            else
+            {
+                lblMessage.Text = "You must input a number to validate!!!";
+            }
+        }
+
+        private void ResetDifficulty()
+        {
+            rbEasy.IsChecked = false;
+            rbMedium.IsChecked = false;
+            rbHard.IsChecked = false;
+            rbVeryHard.IsChecked = false;
+        }
+
+        private void EnableDifficulty(bool isActive)
+        {
+            rbEasy.IsEnabled = isActive;
+            rbMedium.IsEnabled = isActive;
+            rbHard.IsEnabled = isActive;
+            rbVeryHard.IsEnabled = isActive;
+        }
+
+        #region Generate number to guess
+
+        private int GenerateNumber()
+        {
+            Random generator = new Random();
+            if (rbEasy.IsChecked.Value)
+            {
+                return generator.Next(1, 10);
+            }
+            else if (rbMedium.IsChecked.Value)
+            {
+                return generator.Next(1, 100);
+            }
+            else if (rbHard.IsChecked.Value)
+            {
+                return generator.Next(1, 1000);
+            }
+            else if (rbVeryHard.IsChecked.Value)
+            {
+                return generator.Next(1, 10000);
+            }
+            return 0;
+        }
+
+        #endregion Generate number to guess
+
+        #region Allow only numbers in textbox
+
+        private void txtGuessNumber_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            return !regex.IsMatch(text);
+        }
+
+        #endregion Allow only numbers in textbox
     }
 }
